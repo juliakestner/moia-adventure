608a847be5a30f995d8bd0b50c6f4a8f73721041
Julia <julia.kestner@moia.io>
2021-02-20 02:32:17 +0100

    tiles tiles tiles

---
+++ tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="bigTileset" tilewidth="32" tileheight="32" tilecount="2560" columns="80">
+ <image source="bigtileset.png" width="2560" height="1024"/>
+</tileset>
